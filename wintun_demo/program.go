00e3a78aea6bada2b24a635981c66e56a1b79300
cliven <quanguanyu@qq.com>
2020-10-26 17:39:27 +0800

    增加了创建成功提示

--- wintun_demo/program.go
+++ wintun_demo/program.go
@@ -3,18 +3,21 @@ package main
 import (
 	"fmt"
 	"golang.zx2c4.com/wireguard/tun"
+	"log"
 )
 
 func tunInterfaceCreate() (tun.Device, error) {
-	infName := "Hell"
-	tun, err := tun.CreateTUN(infName, 0)
+	infName := "GoWinTun"
+	deivce, err := tun.CreateTUN(infName, 0)
+	//tun, err := tun.CreateTUNWithRequestedGUID(infName, "",0)
 	if err == nil {
-		realName, err := tun.Name()
+		realName, err := deivce.Name()
 		if err == nil {
 			infName = realName
+			log.Printf("Wintun设备 [%s] 创建成功", realName)
 		}
 	} else {
 		return nil, fmt.Errorf("failed to tunInterfaceCreate TUN device: %v", err)
 	}
-	return tun, nil
+	return deivce, nil
 }
